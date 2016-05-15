dff9d25c21bc847ab23d71b68ac4f8f5c1165178
Bror Brurberg <brorbrurberg@me.com>
2016-05-15 20:13:18 +0200

    Webeksamen init

---
+++ brukerside/index.php
@@ -0,0 +1,21 @@
+<!doctype html>
+<head>
+ 
+    <meta charset="utf-8">
+    
+    <!-- stilark -->
+    
+    <!-- Latest compiled and minified CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css"
+    integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
+    
+    <link rel="stylesheet" href="css/style.css">   
+    
+    <!-- stilark -->
+    
+</head>
+<html>
+    <body>
+        
+    </body>    
+</html>
