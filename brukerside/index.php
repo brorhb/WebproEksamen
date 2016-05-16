dad942ac37b92e251c3396dad34fcd380b9e3630
Bror <brorbrurberg@me.com>
2016-05-16 14:21:50 +0200

    Grunnmur forside

--- brukerside/index.php
+++ brukerside/index.php
@@ -2,10 +2,67 @@
     include_once "head.php"
 ?>
 
+<!-- Header bilde -->
+    <div class="row headerBilde"></div>
+<!-- Header bilde -->
+
+<!-- Innhold -->
 <div class="container">
-    <h1>Hello World</h1>
-    <button class="btn btn-default">Test jQuery</button>
+    
+    <!-- Bestille reise -->
+    <div class="row">
+        <div class="col-md-6 col-md-offset-3 bestillReiseBoks">
+            
+        </div>
+    </div>
+    <!-- Bestille reise -->
+    
+    <!-- Populære reiser -->
+    <div class="row">
+        <div class="col-md-6 col-md-offset-3">
+            <h2>Populære reiser</h2>
+            <p>I tvil på hvor du vil reise? Her en liten oversikt over populære reisemål</p>
+            <table class="table">
+                <thead>
+                    <tr>
+                        <th><h4>Land</h4></th>
+                        <th><h4>Flyplass</h4></th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td><strong>Polen</strong></td>
+                        <td>Krakow</td>
+                    </tr>
+                    <tr>
+                        <td><strong>Polen</strong></td>
+                        <td>Krakow</td>
+                    </tr><tr>
+                        <td><strong>Polen</strong></td>
+                        <td>Krakow</td>
+                    </tr><tr>
+                        <td><strong>Polen</strong></td>
+                        <td>Krakow</td>
+                    </tr><tr>
+                        <td><strong>Polen</strong></td>
+                        <td>Krakow</td>
+                    </tr><tr>
+                        <td><strong>Polen</strong></td>
+                        <td>Krakow</td>
+                    </tr><tr>
+                        <td><strong>Polen</strong></td>
+                        <td>Krakow</td>
+                    </tr><tr>
+                        <td><strong>Polen</strong></td>
+                        <td>Krakow</td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+    </div>
+    <!-- Populære reiser -->
 </div>
+<!-- Innhold -->
 
 
 <?php
