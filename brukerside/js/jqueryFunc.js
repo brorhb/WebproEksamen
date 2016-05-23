9bf88f6f66c5f10a2e8e6bf607b7f8e359b3d029
Bror <brorbrurberg@me.com>
2016-05-23 10:17:28 +0200

    lagt til registrer reisende

--- brukerside/js/jqueryFunc.js
+++ brukerside/js/jqueryFunc.js
@@ -52,7 +52,24 @@ $(document).ready(function () {
             $("#reiseEndring").toggle();
         });
     }
-});
+    
+    //legg til/fjern personregistrering
+    var max_fields = 10;
+    var wrapper = $('.formWrapper');
+    var leggTil = $('.legg_til_reisende');
+    var x = 1 //start antall reisende
+    
+    $(leggTil).click(function(e) {
+        e.preventDefault();
+        if( x < max_fields ) {
+            x++
+            $(wrapper).append( 
+                "<div class='reisende'><div class='col-md-12'><h3>Reisende</h3></div> <div class='col-md-6'>                    <label for='fornavn'>Fornavn</label>                    <input type='text' class='form-control' name='fornavn' id='fornavn' placeholder='Fornavn' required />                </div>                <div class='col-md-6'>                    <label for='etternavn'>Etternavn</label>                    <input type='text' class='form-control' name='etternavn' id='etternavn' placeholder='Etternavn' required />                </div>                <div class='col-md-6'>                    <label for='epost'>Epost</label>                    <input type='email' class='form-control' name='email' id='email' placeholder='eksempel@bjarvin.no' required />                </div>                <div class='col-md-6'>                    <label for='kjonn'>Kjønn</label>                    <select class='form-control' name='kjonn' id='kjonn'>                        <option disabled selected value=''>Kjønn</option>                        <option value='1'>Mann</option>                       <option value='2'>Kvinne</option>                    </select>                </div>                <div class='col-md-6'>                    <label for='mobilnr'>Mobilnummer</label>                    <input type='text' class='form-control' name='mobilnummer' id='mobilnummer' placeholder='99999999' required />                </div>                 <div class='col-md-6'>                    <label for='dob'>Fødselsdato</label>                    <input type='text' class='form-control' name='dob' id='dob' placeholder='dd.mm.yyyy' required />                </div></div>"
+            );
+        }
+    });
+    
+}); //Slutt på Jquery
 
 /* Bestilling validering */
 function validerBestilling() {
---
+++ brukerside/registrerReisende.php
@@ -0,0 +1,26 @@
+<?php
+    include_once ('head.php');
+?>
+<div class="container" style="margin-top:55px;">
+    <div class="row">
+        <div class="col-md-8 col-md-offset-1">
+            <h2>Registrer reisende</h2>
+        </div>
+        <div class="col-md-2">
+            <h3 class="totaltReisende"><button class="glyphicon glyphicon-plus legg_til_reisende"></button></h3>
+        </div>
+        <div class="col-md-10 col-md-offset-1">
+            <form method="" action="prisside.php">
+                <div class="form-group">
+                    <div class="formWrapper">
+                        
+                    </div>
+                </div>
+                <input type="submit" class="btn btn-default pull-right" value="Bekreft">
+            </form>
+        </div>
+    </div>
+</div>
+<?php
+    include_once ('end.php');
+?>
