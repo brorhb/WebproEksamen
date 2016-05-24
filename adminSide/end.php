a8c494c744dd3e5cf72b7ad93e9bb9ece08dc1de
Bror <brorbrurberg@me.com>
2016-05-24 10:43:06 +0200

    adminside v0.2
    - Begynt grunnmur på admin panel
    - lagd sidepanel
    - lagt inn nødvendige dokumenter

---
+++ adminSide/end.php
@@ -0,0 +1,14 @@
+                            </div> <!-- col-lg-8 -->
+                        </div> <!-- row -->
+                    </div> <!-- container -->
+                </div>
+                <!-- /#page-content-wrapper -->
+            </div>
+            <!-- /#wrapper -->       
+        <br/>
+        <br/>
+        <footer>
+            <div class="col-md-12 text-center"><h4>Eksamen av Thomas Iversen Ramm, Bror Brurberg, Marius Wetterlin, Emilie Borch og Iselin Gjestland</h4></div>
+        </footer>
+     </body>    
+</html>
--- adminSide/index.php
+++ adminSide/index.php
@@ -1,21 +1,16 @@
-<!doctype html>
-<head>
- 
-    <meta charset="utf-8">
-    
-    <!-- stilark -->
-    
-    <!-- Latest compiled and minified CSS -->
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css"
-    integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
-    
-    <link rel="stylesheet" href="css/style.css">   
-    
-    <!-- stilark -->
-    
-</head>
-<html>
-    <body>
-        
-    </body>    
-</html>
+<?php
+    include_once ("head.php");
+?>
+
+
+
+<!-- Innhold -->
+<div class="container">
+
+</div>
+<!-- Innhold -->
+
+
+<?php
+    include_once ("end.php");
+?>
