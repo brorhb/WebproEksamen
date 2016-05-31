f036893d6ba3e69bec520f091955c56d6bf79122
mariiusw <marius.wetterlin@hotmail.com>
2016-05-31 11:42:56 +0200

    skrivefeil rettinger og struktur

--- 1vedlikehold/leggtiltype.php
+++ 1vedlikehold/leggtiltype.php
@@ -1,7 +1,6 @@
-<?php
+<?php include_once("head.php");
     include_once("lib/funksjoner.php");
     //krevInnlogging('0');
-    include_once("head.php");
 
     connectDB();
     $sql = "INSERT INTO type_luftfartoy ('id', 'type') VALUES ('', '$type');";
@@ -17,26 +16,21 @@
     $connectDB()->close();
 ?>
 
-
-
-<!-- Innhold -->
-<div class="col-md-12">
-    <h2>Nytt Fly</h2>
-        <form action="allefly.php" method="POST">
+    <!-- Innhold -->
+    <div class="col-md-12">
+        <h2>Nytt Fly</h2>
+        <form action="allefly.php">
             <div class="col-md-6">
                 <div class="form-group">
                     <lable for="type">Flytype</lable>
-                    <input class="form-control" type="text" placeholder="Helikopter" name="type" id="modell" required>
+                    <input class="form-control" type="text" placeholder="Helikopter" name="type" id="modell" required/>
                 </div>
             </div>
-        <div class="col-md-12">
-            <input type="submit" class="btn btn-info" value="Endre">
-        </div>
-    </form>
-</div>
-<!-- Innhold -->
+            <div class="col-md-12">
+                <input type="submit" class="btn btn-info" value="endre">
+            </div>
+        </form>
+    </div>
+    <!-- Innhold -->
 
-
-<?php
-    include_once ("end.php");
-?>
+<?php include_once ("end.php"); ?>
