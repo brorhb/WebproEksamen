c957ba8416118bed51f6b9ac42da8a7124d24f55
emibor95 <0emilie@live.no>
2016-05-31 10:29:27 +0200

    Endret slik at alle klasser vises

--- 1vedlikehold/alleklasser.php
+++ 1vedlikehold/alleklasser.php
@@ -7,48 +7,63 @@
     $sql = "SELECT * FROM klasse;";
     $result = connectDB()->query($sql);
 
+    if ($_POST['endre']) {
+        # code...
+    }
+
+    elseif ($_POST['slett']) {
+        # code...
+    }
+
+    elseif ($_POST['ny']) {
+        # code...
+    }
+    
+    else {
+        echo '
+        <!-- Innhold -->
+        <div class="col-md-12">
+            <form method="post" action=" ' . $_SERVER['PHP_SELF'] . ' ">
+            <h2>Alle Klasser</h2>
+            <table class="table table-striped">
+                <thead>
+                    <tr>
+                        <th>Valgt</th>
+                        <th>Klassenavn</th>
+                    </tr>
+                </thead>
+                    <tbody>';
+
+                        
+                            if($result->num_rows > 0 ) {
+                                while ($row = $result->fetch_assoc()) {
+
+                                    $id = utf8_encode($row["id"]);
+                                    $type = utf8_encode($row["type"]);
+                                    echo '<tr><td><input type="radio" name="id" value="' . $id . '"></td><td>' . $type . '</td></tr>';
+                                }
+                            }
+                  echo '
+                    
+                     </tbody>
+                    </table>
+                    <div class="col-md-1">
+                        <input type="submit" name="endre" class="btn btn-info" value="Endre" />
+                    </div>
+                    <div class="col-md-1 col-md-offset-4 text-center">
+                        <input type="submit" name="ny" class="btn btn-success" value="Legg til" />
+                    </div>
+                    <div class="col-md-1 col-md-offset-4 pull-right">
+                        <input type="submit" name="slett" href="#" class="btn btn-danger" value="Slett"/>
+                    </div>
+                </form>
+        </div>
+        <!-- Innhold -->';   
+    }
+
+
 ?>
 
-
-
-
-
-<!-- Innhold -->
-<div class="col-md-12">
-    <h2>Alle Klasser</h2>
-    <table class="table table-striped">
-        <thead>
-            <tr>
-                <th>Valgt</th>
-                <th>Klassenavn</th>
-            </tr>
-        </thead>
-        <tbody>
-            <form>
-                <?php
-                    if($result->num_rows > 0 ) {
-                        while ($row = $result->fetch_assoc()) {
-
-                            $id = utf8_encode($row["id"]);
-                            $type = utf8_encode($row["type"]);
-                            echo '<tr><td><input type="radio" name="id" value="' . $id . '"></td><td>' . $type . '</td></tr>';
-                        }
-                    }
-                ?>
-            </form>
-        </tbody>
-    </table>
-    <div class="col-md-1">
-        <a href="endreklasse.php" class="btn btn-info">Endre</a>
-    </div>
-    <div class="col-md-1 col-md-offset-4 text-center">
-        <a href="leggtilklasse.php" class="btn btn-success">Legg til</a>
-    </div>
-    <div class="col-md-1 col-md-offset-4 pull-right">
-        <a href="#" class="btn btn-danger">Slett</a>
-    </div>
-</div>
-<!-- Innhold -->
 
 
 <?php
