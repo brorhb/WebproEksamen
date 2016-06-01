342ed94b034f45a6bc648f3e97e19dbd4e154d04
Bror <brorbrurberg@me.com>
2016-06-01 15:59:06 +0200

    lagt til validering.js

--- 1vedlikehold/head.php
+++ 1vedlikehold/head.php
@@ -12,6 +12,7 @@
         <!-- Scripts -->
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.2/jquery.min.js"></script>
         <script src="js/jqueryFunc.js"></script>
+        <script src="js/validering.js"></script>
         <!-- Scripts -->
 
         <!-- stilark -->
