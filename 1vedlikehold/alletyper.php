dadad4ed90ae0d302f02dc4f6c0a08f270923286
nilesi5 <iselin_gj@hotmail.com>
2016-06-01 12:21:56 +0200

    Fungerer.

--- 1vedlikehold/alletyper.php
+++ 1vedlikehold/alletyper.php
@@ -3,54 +3,126 @@
     //krevInnlogging('0');
     include_once("head.php");
 
-    connectDB();
-    $sql = "SELECT * FROM type_luftfartoy;";
-    $result = connectDB()->query($sql);
-?>
+    
+    if ($_POST['slett']) {
+        $id = @$_POST['id'];
+        if(slettType_luftfartoy($id)) {
+            echo "Informasjonen ble slettet.";
+        }
+        else {
+            echo "Noe galt skjedde...";
+        }
+    }
+    elseif ($_POST['lagre']) {
+        $id = @$_POST['id'];
+        $type = $_POST['type'];
 
+        if(oppdaterType_luftfartoy($id, $type)) {
+            echo "Informasjonen ble oppdatert.";
+        }
+        else {
+            echo "Noe galt skjedde...";
+        }
+    }
+    elseif ($_POST['ny'] || $_POST['endre']) {
+        // Hvis endre eller ny er trykket ned
+        $id = @$_POST['id'];
 
+        echo'    <!-- Innhold -->
+            <form action="' . $_SERVER['PHP_SELF'] . '" id="oppdater" method="post">
+            <div class="col-md-12">';
+                if ($_POST['ny']) {
+                    echo '<h2>Ny type</h2>';
+                }
+                elseif ($_POST['endre']) {
+                    echo '<h2>Endre type</h2>';
+                }
+        echo '
+            <div class="col-md-6">';
+                
+                    connectDB();
+                    $sql = "SELECT * FROM type_luftfartoy WHERE id='$id';";
+                    $result = connectDB()->query($sql);
 
-
-
-<!-- Innhold -->
-<div class="col-md-12">
-    <h2>Alle typer</h2>
-    <table class="table table-striped">
-        <thead>
-            <tr>
-                <th>Valgt</th>
-                <th>ID</th>
-                <th>Type</th>
-            </tr>
-        </thead>
-        <tbody>
-            <form>
-                <?php
                     if($result->num_rows > 0 ) {
                         while ($row = $result->fetch_assoc()) {
                             $id = utf8_encode($row["id"]);
                             $type = utf8_encode($row["type"]);
-                            echo '<tr><td><input type="radio" name="id" value="' . $id . '"></td><td>' . $id . '</td><td>' . $type . '</td></tr>';
+                            echo '
+                            <div class="form-group">
+                                <lable for="type">type</lable>
+                                <input class="form-control" type="text" placeholder="type" name="type" id="type" value="' . @$type . '" required>
+                                <input class="form-control" type="hidden" placeholder="ID" name="id" id="id" value="' . @$id . '">
+                            </div>';
                         }
                     }
-                ?>
-            </form>    
-            
-        </tbody>
-    </table>
-    <div class="col-md-1">
-        <a href="endretype.php" class="btn btn-info">Endre</a>
-    </div>
-    <div class="col-md-1 col-md-offset-4 text-center">
-        <a href="leggtiltype.php" class="btn btn-success">Legg til</a>
-    </div>
-    <div class="col-md-1 col-md-offset-4 pull-right">
-        <a href="#" class="btn btn-danger">Slett</a>
-    </div>
-</div>
-<!-- Innhold -->
+                    else {
+                        echo '
+                            <div class="form-group">
+                                <lable for="type">type</lable>
+                                <input class="form-control" type="text" placeholder="type" name="type" id="type" value="' . @$type . '" required>
+                                <input class="form-control" type="hidden" placeholder="ID" name="id" id="id" value="' . @$id . '">
+                            </div>';
+                    }
+                    connectDB()->close();
+            echo'
+            </div>
+               <div class="col-md-12">
+                    <input type="submit" id="lagre" name="lagre" class="btn btn-info" value="lagre">
+                </div>
+            </div>
+            </form>
+            <!-- Innhold -->';
+
+    }
+    
+
+        echo'<div class="col-md-12">
+            <form method="post" action="' . $_SERVER['PHP_SELF'] . '">
+            <h2>Alle typer</h2>
+            <table class="table table-striped">
+                <thead>
+                    <tr>
+                        <th>Valgt</th>
+                        <th>Type</th>
+                    </tr>
+                </thead>
+                    <tbody>
+        ';
+                            connectDB();
+                            $sql = "SELECT * FROM type_luftfartoy;";
+                            $result = connectDB()->query($sql);
+
+                            if($result->num_rows > 0 ) {
+                                while ($row = $result->fetch_assoc()) {
+
+                                    $id = utf8_encode($row["id"]);
+                                    $type = utf8_encode($row["type"]);
+                                    echo '<tr><td><input type="radio" name="id" value="' . $id . '"></td><td>' . $type . '</td></tr>';
+                                }
+                            }
+                        
+        echo '
+                     </tbody>
+                    </table>
+                    <div class="col-md-1">
+                        <input type="submit" name="endre" class="btn btn-info" value="Endre" />
+                    </div>
+                    <div class="col-md-1 col-md-offset-4 text-center">
+                        <input type="submit" name="ny" class="btn btn-success" value="Legg til" />
+                    </div>
+                    <div class="col-md-1 col-md-offset-4 pull-right">
+                        <input type="submit" name="slett" href="#" class="btn btn-danger" value="Slett"/>
+                    </div>
+                </form>
+        </div>
+        <!-- Innhold -->
+        ';
+
+    
 
 
-<?php
+
+
     include_once ("end.php");
 ?>
