474093ebee89a6dc15b23dd456c62853298ec99d
mariiusw <marius.wetterlin@hotmail.com>
2016-06-01 17:10:49 +0200

    fix

--- 1vedlikehold/allebrukere.php
+++ 1vedlikehold/allebrukere.php
@@ -52,14 +52,14 @@
 
                     if($result->num_rows > 0 ) {
                         while ($row = $result->fetch_assoc()) {
-                            $id = @$_POST['id'];
-                            $passord = $_POST['passord'];
-                            $fornavn = $_POST['fornavn'];
-                            $etternavn = $_POST['etternavn'];
-                            $dob = $_POST['fodselsdato'];
-                            $land = $_POST['land_id'];
-                            $epost = $_POST["epost"];
-                            $mobil = $_POST["mobilnr"];
+                            $id = utf8_encode($row["id"]);
+                            $passord = utf8_encode($row["passord"]);
+                            $fornavn = utf8_encode($row["fornavn"]);
+                            $etternavn = utf8_encode($row["etternavn"]);
+                            $dob = utf8_encode($row["fodselsdato"]);
+                            $land = utf8_encode($row["land_id"]);
+                            $epost = utf8_encode($row["epost"]);
+                            $mobil = utf8_encode($row["mobilnr"]);
                             echo '
                             <div class="form-group col-md-6">
                                 <lable for="brukerID">Bruker ID</lable>
@@ -77,7 +77,9 @@
                                 <lable for="fodselsdato">Fodselsdato</lable>
                                 <input class="form-control" type="text" placeholder="Fodselsdato" name="fodselsdato" id="fodselsdato" value="' . @$dob . '">
                                 <lable for="land">Land</lable>
-                                <input class="form-control" type="text" placeholder="Land" name="land" id="land" value="' . @$land . '" required>
+                                '; 
+                            echo landListe();
+                            echo '
                             </div>
                             <div class="form-group col-md-6">
                                 <lable for="epost">E-post</lable>
@@ -104,8 +106,9 @@
                             <div class="form-group col-md-6">
                                 <lable for="fodselsdato">Fodselsdato</lable>
                                 <input class="form-control" type="text" placeholder="Fodselsdato" name="fodselsdato" id="fodselsdato" value="' . @$dob . '">
-                                <lable for="land">Land</lable>
-                                <input class="form-control" type="text" placeholder="Land" name="land" id="land" value="' . @$land . '" required>
+                                <lable for="land">Land</lable>';
+                        echo landListe();
+                        echo '
                             </div>
                             <div class="form-group col-md-6">
                                 <lable for="epost">E-post</lable>
