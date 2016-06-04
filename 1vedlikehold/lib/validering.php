33cfc4fd116d8dbf2ab6f4293129a9c00e410d9c
rammt <thomasiversenramm@Thomass-MacBook-Pro-3.local>
2016-06-04 02:46:53 +0200

    Lagt til validerBruker() funksjon

--- 1vedlikehold/lib/validering.php
+++ 1vedlikehold/lib/validering.php
@@ -1,6 +1,6 @@
 <?php
 
-    include_once("funksjoner.php");
+	include_once("funksjoner.php");
 
 	// Eksempel på valider-funksjon
 	function validerEksempel() {
@@ -110,40 +110,119 @@
 		echo $output;
 	}
 
-	function validerBruker($BrukerID, $brukernavn, $epost, $ukryptert_passord, $land_id, $mobilnr, $person_id) {
+	function validerBruker($BrukerID, $brukernavn, $epost, $ukryptert_passord, $land_id, $mobilnummer, $person_id) {
+		/* Klarerer variabler */
+		$maaFyllesUt = array();
+		$kommentar = array();
+		$resultat = true;
+
 		$id = utf8_decode($BrukerID);
 		$brukernavn = utf8_decode($brukernavn);
 		$epost = utf8_decode($epost);
 		$ukryptert_passord = utf8_decode($ukryptert_passord);
 		$land_id = utf8_decode($land_id);
-		$mobilnr = utf8_decode($mobilnr);
+		$mobilnummer = utf8_decode($mobilnummer);
 		$person_id = utf8_decode($person_id);
 
-		$tilbakemelding = array();
-
-		if (!is_numeric($id) AND $id !== 'IGNORE') {
-			$tilbakemelding[] = 'BrukerIDen må kun bestå av tall.';
+		/* Ulike valideringer */
+		
+		// Sjekker om feltet er tomt
+		if ($id == "" || $id == null) {
+			$maaFyllesUt[] = "brukerID";
+			$resultat = false;
 		}
-		if (strlen($brukernavn) < 2 AND $brukernavn !== 'IGNORE') {
-			$tilbakemelding[] = 'Brukernavnet må være minimum to tegn.';
-		}
-		if (filter_var($epost, FILTER_VALIDATE_EMAIL) === false AND $epost !== 'IGNORE') {
-			$tilbakemelding[] = 'Epostadressen er ugyldig.';
-		}
-		if (strlen($ukryptert_passord) < 5 AND $ukryptert_passord !== 'IGNORE') {
-			$tilbakemelding[] = 'Passordet må inneholde minst 5 tegn.';
-		}
-		if (!is_numeric($land_id) AND $land_id !== 'IGNORE') {
-			$tilbakemelding[] = 'LandIDen må kun bestå av tall.';
-		}
-		if ((!is_numeric($mobilnr) OR strlen($mobilnr) < 8) AND $mobilnr !== 'IGNORE') {
-			$tilbakemelding[] = 'Mobilnummeret må bestå av minimum 8 tegn og kun være siffer.';
-		}
-		if (!is_numeric($person_id) AND $person_id !== 'IGNORE') {
-			$tilbakemelding[] = 'PersonIDen må kun bestå av tall.';
+		// Feltet er fylt ut, sjekker ytterligere valideringer
+		else {
+			if (!is_numeric($id)) {
+				$kommentar[] = "<strong>BrukerID</strong>en må kun bestå av tall.";
+				$resultat = false;
+			}
 		}
 
-		return $tilbakemelding;
+		// Sjekker om feltet er tomt
+		if ($brukernavn == "" || $brukernavn == null) {
+			$maaFyllesUt[] = "brukernavn";
+			$resultat = false;
+		}
+
+		// Sjekker om feltet er tomt
+		if ($epost == "" || $epost == null) {
+			$maaFyllesUt[] = "epost";
+			$resultat = false;
+		}
+		// Feltet er fylt ut, sjekker ytterligere valideringer
+		else {
+			if (filter_var($epost, FILTER_VALIDATE_EMAIL) === false) {
+				$kommentar[] = "<strong>Epostadressen</strong> er ugyldig.";
+				$resultat = false;
+			}
+		}
+
+		// Sjekker om feltet er tomt
+		if ($ukryptert_passord == "" || $ukryptert_passord == null) {
+			$maaFyllesUt[] = "passord";
+			$resultat = false;
+		}
+		// Feltet er fylt ut, sjekker ytterligere valideringer
+		else {
+			if (strlen($ukryptert_passord) < 5) {
+				$kommentar[] = "<strong>Passordet</strong> må inneholde minumum 5 tegn.";
+				$resultat = false;
+			}
+		}
+
+		// Sjekker om feltet er tomt
+		if ($land_id == "" || $land_id == null) {
+			$maaFyllesUt[] = "land";
+			$resultat = false;
+		}
+		// Feltet er fylt ut, sjekker ytterligere valideringer
+		else {
+			if (sjekkOmLandIDeksisterer($land_id)) {
+				$kommentar[] = "<strong>Landet</strong> eksisterer ikke.";
+				$resultat = false;
+			}
+		}
+
+		// Sjekker om feltet er tomt
+		if ($mobilnummer == "" || $mobilnummer == null) {
+			$maaFyllesUt[] = "mobilnummer";
+			$resultat = false;
+		}
+		// Feltet er fylt ut, sjekker ytterligere valideringer
+		else {
+			if (is_numeric($mobilnummer)) {
+				$kommentar[] = "<strong>Mobilnummeret</strong> kan kun inneholde siffer fra 0 - 9.";
+				$resultat = false;
+			}
+			if (strlen($mobilnummer) < 8) {
+				$kommentar[] = "<strong>Mobilnummeret</strong> må inneholde minimum 8 tegn.";
+				$resultat = false;
+			}
+		}
+
+		// Sjekker om feltet er tomt
+		if ($person_id == "" || $person_id == null) {
+			$maaFyllesUt[] = "person";
+			$resultat = false;
+		}
+		// Feltet er fylt ut, sjekker ytterligere valideringer
+		else {
+			if (sjekkOmPersonIDeksisterer($person_id)) {
+				$kommentar[] = "<strong>Personen</strong> eksisterer ikke.";
+				$resultat = false;
+			}
+		}
+
+		/* Valideringer slutt */
+		
+		// Skriver ut feilmeldingsboks
+		if (!$resultat) {
+			feilmeldingBoks($maaFyllesUt, $kommentar);
+		}
+		
+		// Returnerer om neste side skal lastes inn
+		return $resultat;
 	}
 
 ?>
