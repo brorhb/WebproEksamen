57c3ac1f31cacfb4a13cb53f797d0a07001acc66
emibor95 <0emilie@live.no>
2016-06-07 14:21:30 +0200

    php validering reisende

--- 1nettsted/validering.php
+++ 1nettsted/validering.php
@@ -65,4 +65,94 @@
 
 	// Eksempel på valider-funksjon slutt
 
+	/* Validering php Registrer reisende*/
+function validerRegistrerReisende($fornavn,$etternavn, $epost, $kjonn, $fodeselsdato, $mobilnummer) {
+
+		/* Klarerer variabler */
+		$maaFyllesUt = array();
+		$kommentar = array();
+		$resultat = true;
+
+		$fornavn = utf8_decode($fornavn);
+		$etternavn = utf8_decode($etternavn);
+		$epost = utf8_decode($epost);
+		$kjonn = utf8_decode($kjonn);
+		$fodeselsdato = utf8_decode($fodeselsdato);
+		$mobilnummer = utf8_decode($mobilnummer);
+
+		/* Ulike valideringer */
+		
+		// Sjekker om feltet er tomt
+		if ($fornavn == "" || $fornavn == null) {
+			$maaFyllesUt[] = "fornavn";
+			$resultat = false;
+		}
+
+		if ($etternavn == "" || $etternavn == null) {
+			$maaFyllesUt[] = "etternavn";
+			$resultat = false;
+		}
+
+		if ($epost == "" || $epost == null) {
+			$maaFyllesUt[] = "epost";
+			$resultat = false;
+		}
+		// Feltet er fylt ut, sjekker ytterligere valideringer
+		else {
+			if (filter_var($epost, FILTER_VALIDATE_EMAIL) === false) {
+				$kommentar[] = "<strong>E-postadressen</strong> er ugyldig.";
+				$resultat = false;
+			}
+			if (sjekkOmEpostEksisterer($epost) ) {
+				$kommentar[] = "<strong>E-postadressen</strong> eksisterer.";
+				$resultat = false;
+			}
+		}
+		
+		if ($kjonn == "" || $kjonn== null) {
+			$maaFyllesUt[] = "kjønn";
+			$resultat = false;
+		}
+
+		if ($mobilnummer == "" || $mobilnummer == null) {
+			$maaFyllesUt[] = "mobilnummer";
+			$resultat = false;
+		}
+		// Feltet er fylt ut, sjekker ytterligere valideringer
+		else {
+			if (!is_numeric($mobilnummer)) {
+				$kommentar[] = "<strong>Mobilnummeret</strong> må inneholde minimum 8 siffer.";
+				$resultat = false;
+			}
+			
+			if (sjekkOmMobilnummerEksisterer($mobilnummer)) {
+				$kommentar[] = "<strong>Mobilnummer</strong> eksisterer.";
+				$resultat = false;
+			}
+		}
+		if ($fodselsdato == "" || $fodselsdato == null) {
+			$maaFyllesUt[] = "fødselsdato";
+			$resultat = false;
+		}
+		// Feltet er fylt ut, sjekker ytterligere valideringer
+		else {
+			if ($fodselsdato > time()) {
+				$kommentar[] = "passasjer må være født";
+				$resultat = false;
+			}
+		}
+
+			
+		/* Valideringer slutt */
+		
+		// Skriver ut feilmeldingsboks
+		if (!$resultat) {
+			feilmeldingBoks($maaFyllesUt, $kommentar);
+		}
+		
+		// Returnerer om neste side skal lastes inn
+		return $resultat;
+	}
+
+
 	?>
