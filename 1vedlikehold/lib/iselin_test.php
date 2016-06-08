17c85aeff436069a3aa03149c68b065e07a419fd
nilesi5 <iselin_gj@hotmail.com>
2016-06-09 00:13:11 +0200

    rettet

---
+++ 1vedlikehold/lib/iselin_test.php
@@ -0,0 +1,31 @@
+<?php
+	session_start();
+
+	include_once("funksjoner.php");
+	//krevInnlogging('0');
+
+	if (!erLoggetInn()) {
+		echo '<a href="../login.php"<h2>Logg inn</h2></a>';
+	}
+	else {
+		echo '<a href="logg-ut.php"<h2>Logg ut</h2></a>';
+	}
+
+	//echo "Navn: " . HentFulltNavnFraBrukerID(@$_SESSION['brukerID']);
+
+	echo "<br>BrukerID: " . @$_SESSION['brukerID'] . " slutt<br>";
+
+	echo 'Info: ' . HentValutaIDFraLandID('2') . '<br><br>';
+
+	
+	echo "<br><br>";
+
+	
+	$test = sjekkOmLuftfartoyIDeksistereriFlyvning("2");
+
+	if ($test) {
+		echo "Returnerer true (" . $test . ")";
+	}
+	else {
+		echo "Returnerer false (" . $test . ")";
+	}
